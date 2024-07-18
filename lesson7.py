9c91f8e3bffb17188ef0b0c66912689d2a9943dc
YOUR USERNAME ON GITHUB <YOUR EMAIL ON GITHUB>
2024-07-18 12:41:57 +0600

    hw6

--- lesson7.py
+++ lesson7.py
@@ -1,52 +1,52 @@
-# # " БД-База Данных"
-# # "СУДБ-Система управление базой данных "
-# # "CRUD- Create,Reade,Update,Delete"
+# " БД-База Данных"
+# "СУДБ-Система управление базой данных "
+# "CRUD- Create,Reade,Update,Delete"
 
-# # import sqlite3
+# import sqlite3
 
-# # connect = sqlite3.connect("instagram.db")
-# # cursor = connect.cursor()
+# connect = sqlite3.connect("instagram.db")
+# cursor = connect.cursor()
 
-# # cursor.execute("""
-# #                CREATE TABLE IF NOT EXISTS instagram(
-# #                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-# #                    fuul_name VARCHAR (30) NOT NULL,
-# #                    username VARCHAR (50),
-# #                    age INT DEFAULT NULL,
-# #                    birth_date DATE,
-# #                    raiting DOUBLE(4,2) DEFAULT 0.0,
-# #                    is_activ BOOLEAN NOT NULL DEFAULT FALSE
+# cursor.execute("""
+#                CREATE TABLE IF NOT EXISTS instagram(
+#                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                    fuul_name VARCHAR (30) NOT NULL,
+#                    username VARCHAR (50),
+#                    age INT DEFAULT NULL,
+#                    birth_date DATE,
+#                    raiting DOUBLE(4,2) DEFAULT 0.0,
+#                    is_activ BOOLEAN NOT NULL DEFAULT FALSE
                    
-# #                )""")
+#                )""")
 
 
-# # def register():
-# #     full_name=input("Введите ФИО:")
-# #     username=input("Введите свой username:")
-# #     age=int(input("Введите возраст:"))
-# #     birth_date=input("Введите дату рождения:")
-# #     raiting=float(input("Оцените приложение:"))
-# #     is_activ=bool(input("Принятия соглашения о конфедициальности"))
+# def register():
+#     full_name=input("Введите ФИО:")
+#     username=input("Введите свой username:")
+#     age=int(input("Введите возраст:"))
+#     birth_date=input("Введите дату рождения:")
+#     raiting=float(input("Оцените приложение:"))
+#     is_activ=bool(input("Принятия соглашения о конфедициальности"))
     
-# #     # cursor.execute("""INSERT INTO instagram
-# #     #               ( full_name,username,age,birth_date,raiting,is_activ)
-# #     #                VALUES('?','?','?','?','?','?')""",full_name,username,age,birth_date,raiting,is_activ)
+#     # cursor.execute("""INSERT INTO instagram
+#     #               ( full_name,username,age,birth_date,raiting,is_activ)
+#     #                VALUES('?','?','?','?','?','?')""",full_name,username,age,birth_date,raiting,is_activ)
     
    
-# #     cursor.execute("""INSERT INTO instagram
-# #                   ( full_name,username,age,birth_date,raiting,is_activ)
-# #                    VALUES('{fulname}','?','?','?','?','?')""",full_name,username,age,birth_date,raiting,is_activ)
+#     cursor.execute("""INSERT INTO instagram
+#                   ( full_name,username,age,birth_date,raiting,is_activ)
+#                    VALUES('{fulname}','?','?','?','?','?')""",full_name,username,age,birth_date,raiting,is_activ)
     
      
-# #     connect.commit()
+#     connect.commit()
     
-# # def all_student():
-# #     cursor.execute("SELECT*FROM instagram")
-# #     student=cursor.fetchall()
-# #     print(student)
+# def all_student():
+#     cursor.execute("SELECT*FROM instagram")
+#     student=cursor.fetchall()
+#     print(student)
     
     
-# # register()
+# register()
     
 # '''1.  git config --global user.name "YOUR USERNAME ON GITHUB"
 #    2.  git config --global user.email "YOUR EMAIL ON GITHUB"'''
